dd41aef3e0503cf23985846724238fda97116168
James Gould <james-gould@users.noreply.github.com>
2025-07-11 15:04:15 +0100

    Corrected gitignore for migration persistence

---
+++ ProjectBase.Data/Migrations/20250711140248_ce26a889-d0fd-45f2-b4ca-034fac177fbc.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectBase.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ce26a889d0fd45f2b4ca034fac177fbc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeatherItems",
+                columns: table => new
+                {
+                    WeatherItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Reading = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherItems", x => x.WeatherItemId);
+                    table.ForeignKey(
+                        name: "FK_WeatherItems_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "CityId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherItems_CityId",
+                table: "WeatherItems",
+                column: "CityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherItems");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
